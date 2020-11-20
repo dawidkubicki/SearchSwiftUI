e45db03622ecfbc71f7124c9b1b41ebd7a421805
Dawid Kubicki <dawid@pluscode.io>
2020-11-20 12:23:22 +0100

    Initial Commit

---
+++ SearchSwiftUI/SearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIApp.swift
+//  SearchSwiftUI
+//
+//  Created by Dawid Kubicki on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
